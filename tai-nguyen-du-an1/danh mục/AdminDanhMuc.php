172c09cc83cee32c0ca06b00d58a302918082138
H-I-E-Uu <hieupmph50358@gmail.com>
2024-11-11 21:09:56 +0700

    Add files via upload

---
+++ tai-nguyen-du-an1/danh mục/AdminDanhMuc.php
@@ -0,0 +1,99 @@
+<?php
+class AdminDanhMuc
+{
+    public $conn;
+
+    public function __construct()
+    {
+        $this->conn = connectDB();
+    }
+
+    public function getAllDanhMuc()
+    {
+        try {
+            $sql = 'SELECT * FROM danh_mucs';
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo 'Lỗi' . $e->getMessage();
+        }
+    }
+
+    public function insertDanhMuc($ten_danh_muc, $mo_ta, $trang_thai)
+    {
+        try {
+            $sql = 'INSERT INTO danh_mucs (ten_danh_muc, mo_ta, trang_thai)
+            VALUES (:ten_danh_muc, :mo_ta, :trang_thai)';
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute([
+                ':ten_danh_muc' => $ten_danh_muc,
+                ':mo_ta' => $mo_ta,
+                ':trang_thai' => $trang_thai
+            ]);
+
+            return true;
+        } catch (Exception $e) {
+            echo 'Lỗi' . $e->getMessage();
+        }
+    }
+
+    public function getDetailDanhMuc($id)
+    {
+        try {
+            $sql = 'SELECT * FROM danh_mucs WHERE id = :id';
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute([
+                ':id' => $id
+            ]);
+
+            return $stmt->fetch();
+        } catch (Exception $e) {
+            echo 'Lỗi' . $e->getMessage();
+        }
+    }
+
+    public function updateDanhMuc($id, $ten_danh_muc, $mo_ta, $trang_thai)
+    {
+        try {
+            $sql = 'UPDATE danh_mucs SET ten_danh_muc =:ten_danh_muc, mo_ta =:mo_ta, trang_thai =:trang_thai  WHERE id = :id';
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute([
+                ':ten_danh_muc' => $ten_danh_muc,
+                ':mo_ta' => $mo_ta,
+                ':id' => $id,
+                ':trang_thai' => $trang_thai
+            ]);
+
+            return true;
+        } catch (Exception $e) {
+            echo 'Lỗi' . $e->getMessage();
+        }
+    }
+
+    public function destroyDanhMuc($id)
+    {
+        try {
+            $sql = 'DELETE FROM danh_mucs WHERE id = :id';
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute([
+                ':id' => $id
+            ]);
+
+            return true;
+        } catch (Exception $e) {
+            echo 'Lỗi' . $e->getMessage();
+        }
+    }
+}
