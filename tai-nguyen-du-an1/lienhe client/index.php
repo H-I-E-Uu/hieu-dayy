fccb0631d7961330927f99b016162d33fd2c4d8e
H-I-E-Uu <hieupmph50358@gmail.com>
2024-11-21 15:19:29 +0700

    Add files via upload

---
+++ tai-nguyen-du-an1/lienhe client/index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+// require_once './controllers/TaiKhoanController.php';
+require_once './controllers/HomeController.php';
+require_once './controllers/TaiKhoanController.php';
+require_once './controllers/LienHeController.php'; // Controller liên hệ
+
+// Require toàn bộ file Models
+require_once './models/SanPham.php';
+require_once './models/TaiKhoan.php';
+require_once './models/GioHang.php';
+require_once './models/LienHe.php'; // Model liên hệ
+
+
+// Route
+$act = $_GET['act'] ?? '/';
+// var_dump($_GET['act']);die();
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Trang chủ
+    '/' => (new HomeController())->home(),
+    // Trường hợp đặc biệt
+
+
+    'chi-tiet-san-pham' => (new HomeController())->chiTietSanPham(),
+    // Base URL/?act=dnah-sach-san-pham
+    'them-gio-hang' => (new HomeController())->addGioHang(),
+    'gio-hang' => (new HomeController())->gioHang(),
+
+    // +
+    'login' => (new HomeController())->formLogin(),
+    'check-login' => (new HomeController())->postLogin(),
+    'logout' => (new HomeController())->Logout(),
+
+
+    'list-tai-khoan' => (new TaiKhoanController())->danhSach(),
+    'form-them' => (new TaiKhoanController())->formAdd(),
+    'them' => (new TaiKhoanController())->postAdd(),
+
+    // Quản lý liên hệ
+    'form-them-lien-he' => (new LienHeController())->formAdd(), // Hiển thị form thêm liên hệ
+    'them-lien-he' => (new LienHeController())->postAdd(),      // Xử lý thêm liên hệ
+// 'danh-sach-lien-he' => (new LienHeController())->danhSach() 
+};
